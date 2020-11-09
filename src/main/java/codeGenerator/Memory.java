e21b29eb4773df60bc7738069c601cc0ed9750e4
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-11-09 21:56:00 +0330

    Making final fields static

--- src/main/java/codeGenerator/Memory.java
+++ src/main/java/codeGenerator/Memory.java
@@ -10,10 +10,10 @@ public class Memory {
     private List<_3AddressCode> codeBlock;
     private int lastTempIndex;
     private int lastDataAddress;
-    private final int stratTempMemoryAddress = 500;
-    private final int stratDataMemoryAddress = 200;
-    private final int dataSize = 4;
-    private final int tempSize = 4;
+    private final static int stratTempMemoryAddress = 500;
+    private final static int stratDataMemoryAddress = 200;
+    private final static int dataSize = 4;
+    private final static int tempSize = 4;
 
     public Memory() {
         codeBlock = new ArrayList<_3AddressCode>();
